6c67b53004ade90347cb36f9f9e3ae3e17493005
Dilip138 <dilipsoni199566@gmail.com>
2020-01-24 12:37:05 +0530

    [Dilip]Refactor. buisness logic of app.css and add a new component dashBoard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Admin from './components/login';
+import DashBoard from './components/dashBoardComponent';
 import './App.css';
 function App() {
   return (
       <Router>
         <Switch>
           <Route path="/" exact={true} component={Admin}></Route>
+          <Route path="/dashBoardComponent"component={DashBoard}></Route>
         </Switch>
       </Router>
   );
---
+++ src/components/dashBoardComponent.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { createMuiTheme, MuiThemeProvider, Divider } from '@material-ui/core';
+const theme = createMuiTheme({
+    overrides: {
+        MuiAppBar: {
+            colorPrimary: {
+                backgroundColor: "lightcoral"
+            }
+        },
+    }
+})
+
+export default class DashBoard extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="mainDashboard">
+                <MuiThemeProvider theme={theme}>
+                    <AppBar position="static">
+                        <Toolbar variant="dense">
+                            <div className="navBar">
+                                <div className="allName">
+                                    <div className="admin">
+                                        ADMIN DASHBOARD
+                            </div>
+                                    <div className="user">
+                                        user
+                            </div>
+                                    <div className="qAndAns">
+                                        Q&A
+                            </div>
+                                </div>
+                                <div className="logOut">
+                                    logOut
+                        </div>
+                            </div>
+                        </Toolbar>
+                    </AppBar>
+                </MuiThemeProvider>
+                <div className="allTable">
+                    <Divider style={{backgroundColor:'blue'}} />
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">FirstName</th>
+                                <th scope="col">LastName</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Service</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>akash</td>
+                                <td >soni</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
